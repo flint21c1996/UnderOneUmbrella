2ec9c1bb7ec7bc39a6214d5e1446cacccb9101a4
seungwoon <usw3473@gmail.com>
2026-05-15 15:58:35 +0900

    [Fix] 블루프린트 함수로 정의할 필요없음

--- Source/UnderOneUmBrella/World/WaterTarget/UOUPuzzleConditionActivatable.cpp
+++ Source/UnderOneUmBrella/World/WaterTarget/UOUPuzzleConditionActivatable.cpp
@@ -1,20 +1,3 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "World/WaterTarget/UOUPuzzleConditionActivatable.h"
-
-bool IUOUPuzzleConditionActivatable::CanActivateByPuzzleCondition_Implementation(const FUOUPuzzleConditionContext& Context) const
-{
-	return true;
-}
-
-void IUOUPuzzleConditionActivatable::ActivateByPuzzleCondition_Implementation(const FUOUPuzzleConditionContext& Context)
-{
-}
-
-void IUOUPuzzleConditionActivatable::DeactivateByPuzzleCondition_Implementation(const FUOUPuzzleConditionContext& Context)
-{
-}
-
-void IUOUPuzzleConditionActivatable::OnPuzzleConditionStateChanged_Implementation(const FUOUPuzzleConditionContext& Context)
-{
-}
