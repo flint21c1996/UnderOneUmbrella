1ff83fd13d9e6bbe245f66256ff1cd6b280dabb4
DESKTOP-UBR3QRS\usw34 <SeungWoon>
2026-08-11 15:31:27 +0900

    [Feat] 퍼즐 치트 코드의 조건부 컴파일에 사용할 공통 매크로의 안전한 기본값을 제공한다.

---
+++ Source/UnderOneUmBrella/Debug/UOUDevelopmentCheatBuild.h
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+// 빌드 규칙에서 명시적으로 허용하지 않은 환경은 개발 치트 코드를 제외합니다.
+#ifndef UOU_WITH_PUZZLE_CHEATS
+#define UOU_WITH_PUZZLE_CHEATS 0
+#endif
