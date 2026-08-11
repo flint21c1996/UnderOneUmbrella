68dbd20687816214a646d51a09e39242e850d1bf
DESKTOP-UBR3QRS\usw34 <SeungWoon>
2026-08-11 17:44:00 +0900

    [Fix] 기존 퍼즐 치트 매크로를 공통 개발 도구 경계에 연결

--- Source/UnderOneUmBrella/Debug/UOUDevelopmentCheatBuild.h
+++ Source/UnderOneUmBrella/Debug/UOUDevelopmentCheatBuild.h
@@ -2,7 +2,9 @@
 
 #pragma once
 
-// 빌드 규칙에서 명시적으로 허용하지 않은 환경은 개발 치트 코드를 제외합니다.
+#include "Debug/UOUDevelopmentToolsBuild.h"
+
+// 기존 퍼즐 치트 코드는 공통 개발 도구 빌드 경계를 따릅니다.
 #ifndef UOU_WITH_PUZZLE_CHEATS
-#define UOU_WITH_PUZZLE_CHEATS 0
+#define UOU_WITH_PUZZLE_CHEATS UOU_WITH_DEVELOPMENT_TOOLS
 #endif
